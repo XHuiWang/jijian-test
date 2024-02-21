9d8f3dc892bb753c3a390d18e30b722bda031edf
wxh <2951576508@qq.com>
2024-02-21 21:28:22 +0800

    add a.sh

---
+++ a.sh
@@ -0,0 +1,7 @@
+git init
+git config --global user.name "wxh"
+git config --global user.email "2951576508@qq.com"
+touch a.txt
+echo "hello,aaaaaa" >> a.txt
+git add a.txt
+git commit -m "add new file a.txt"
